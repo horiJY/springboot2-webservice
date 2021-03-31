7316f5e305f1a09c4881fa2872a341b12d948b2b
horiJY <45135200+horiJY@users.noreply.github.com>
2021-03-31 09:03:07 +0900

    JpaAuditing(생성/수정시간 자동화)

--- src/main/java/com/jy/prac/springboot/Application.java
+++ src/main/java/com/jy/prac/springboot/Application.java
@@ -2,7 +2,9 @@ package com.jy.prac.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication  // 스프링 부트의 자동 설정, 스프링 Bean 일기와 생성을 모두 자동으로 설정됩니다.
 public class Application {
     public static void main(String[] args) {
