84c160a30df300f5f08512f8c580f276cd49ed14
horiJY <45135200+horiJY@users.noreply.github.com>
2021-06-29 04:15:01 +0900

    Travis CI 빌드 문제(스프링 부트, 그레이들 버전낮음) 버전업 Spring Boot 2.1.9 > 2.4.1 Gradle 4.10.2 > 6.7.1 *build.gradle 변경완료

--- src/test/java/com/jy/prac/springboot/web/ProfileControllerUnitTest.java
+++ src/test/java/com/jy/prac/springboot/web/ProfileControllerUnitTest.java
@@ -1,6 +1,6 @@
 package com.jy.prac.springboot.web;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.mock.env.MockEnvironment;
 
 import static org.assertj.core.api.Assertions.assertThat;
